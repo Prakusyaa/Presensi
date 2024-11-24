6e18999766334a512e54375b96ccb300a347ae09
Muhammad Anindya Prakusya <muhammadanindyaprakusya@gmail.com>
2024-11-24 20:36:48 +0700

    Belajar

--- lib/main.dart
+++ lib/main.dart
@@ -25,24 +25,79 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
-      ),
-      body: Container(
-        child: Row(
+        appBar: AppBar(
+          title: const Text('Aplikasi Presensi'),
+        ),
+        body: Row(
           children: [
-            Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("Belajar Flutter")
-                  ],
+            Column(
+              children: [
+                Center(
+                  child: Container(
+                    margin: EdgeInsets.only(top: 10, left: 18),
+                    height: 100,
+                    width: screenWidth * 0.9,
+                    decoration: BoxDecoration(
+                      color: Color.fromRGBO(137, 207, 240, 1),
+                      borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 5,
+                          blurRadius: 10,
+                          offset: Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Divider(height: 40),
+                Container(
+                  margin: EdgeInsets.only(top: 10, left: 18),
+                  height: 535,
+                  width: screenWidth * 0.9,
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(137, 207, 240, 1),
+                    borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 5,
+                        blurRadius: 10,
+                        offset: Offset(0, 3),
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    children: [
+                      Center(
+                        child: Container(
+                          margin: EdgeInsets.only(top: 10),
+                          height: 515,
+                          width: screenWidth * 0.85,
+                          decoration: BoxDecoration(
+                            color: Color.fromRGBO(249, 246, 238, 1),
+                            borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.5),
+                                spreadRadius: 5,
+                                blurRadius: 10,
+                                offset: Offset(0, 3),
+                              ),
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
                 )
+              ],
             )
           ],
-        ),
-      ),
+        )
     );
   }
 }
